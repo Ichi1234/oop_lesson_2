2ea0ff0ce9b999490647efb1e6086a17849be4a2
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2023-11-09 14:13:15 +0700

    create new branch and make gen_comb function

--- data_processing.py
+++ data_processing.py
@@ -203,3 +203,4 @@ male_sur = my_titan.filter(lambda x: x['gender'] == 'M').filter(lambda x: x['sur
 male_len = len(male.table)
 male_sur_len = len(male_sur.table)
 print(f"The survival rate of male {(male_sur_len/male_len)*100}%")
+
